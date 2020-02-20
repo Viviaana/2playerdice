f244553411440224bd056915f9221208379f971e
Katie Ingham <kjingham91@gmail.com>
2020-02-20 17:25:31 +0000

    day1 last push

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let dice1 = Math.floor(Math.random() *6) +1;
 let dice2 = Math.floor(Math.random() *6) +1;
+let scores, roundScore, activePlayer, gamePlaying;
 
 
-const total1
+const total1 = document.getElementById("total1");
+const total2 = document.getElementById("total2");
+const rolled1 = document.getElementsByClassName("rolled1");
